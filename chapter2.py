7407264336796cbda96642ff242a574a79f10b09
Yolanda Gunter <yemgunter@gmail.com>
2021-01-28 10:03:08 -0600

    Continuing Chapter 2 lecture

--- chapter2.py
+++ chapter2.py
@@ -1,3 +1,28 @@
 name = input("Enter your name: ")
 age = '50'
 print('Hi ', name, ', you are ', age, ' years old.', sep='')
+
+Print("Thursday January 28 2021")
+
+name = "Henry"
+name2 = "Archibald"
+
+print("""I said "I'm hungry" """)
+
+hot_dogs_sold_today = 56
+
+x = 5
+
+
+
+print(3)
+
+print()
+
+
+
+Finds out the User Name & Age
+
+name = input("Enter your name: ")
+
+print("Hello,", name, " you are ", age, " years old,")
---
+++ chapter2a.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+
+age = int(input("Enter your age: "))
+
+next_age = age + 1
+
+print("Hello,", name, " you will be", next_age, "years old on your next birthday.")
