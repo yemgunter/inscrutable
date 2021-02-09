dc046f2b9b863d9ae7f7693f3df7f986ec6a3921
Yolanda Gunter <yemgunter@gmail.com>
2021-02-09 10:00:41 -0600

    Lecture Tursday 2/9/21

---
+++ decisions2.py
@@ -0,0 +1,27 @@
+# Short-Circuit Evaluation
+x = 12
+y = 0
+.
+.
+x = 1
+.
+.
+if (x > 5) or ((x // y) >= 3):
+    x = 25
+
+
+
+
+
+
+
+##upper = 50
+##lower = -5
+##
+##num = int(input("Enter an interger: "))
+##
+##if num >=-5 and num <=50:
+##    print("The number is in the range")
+##else:
+##    print("The number is not in range")
+##    
---
+++ tickets.py
@@ -0,0 +1,37 @@
+
+# child is 8 or less $5.00
+# senior is 65 or older $8.00
+# adult $10.00
+
+
+age = int(input("Enter your age: "))
+
+# Assuming user has entered a valid age
+
+
+##if age >= 65:
+##    ticket_price = 8.0
+##else:
+##    if age <= 8:
+##        ticket_price = 5.0
+##    else:
+##        ticket_price = 10.0
+
+
+##if age > 8 and age < 65:
+##    ticket_price = 10.0
+##elif age <= 8:
+##    ticket_price = 5.0
+##else:
+##    ticket_price = 8.0
+    
+
+##if age <= 8:
+##    ticket_price = 5.0
+##elif age < 65:
+##    ticket_price = 10.0
+##else:
+##    ticket_price = 8.0
+##
+
+print("Ticket price is $", format(ticket_price, '.2f'), sep='')
