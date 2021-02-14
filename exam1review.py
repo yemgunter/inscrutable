543e838aaea5c2ebace799a2d17ee3ebcff11760
Yolanda Gunter <yemgunter@gmail.com>
2021-02-13 19:05:43 -0600

    Create exam1review.py

---
+++ exam1review.py
@@ -0,0 +1,13 @@
+
+
+# Short Circuit Evaluation
+# Using OR if the first term is false, it won't go further
+# Using AND it will read both terms
+
+num1 = 23
+num2 = 8
+
+if num1 >= 100 and num2 == 10:
+    print("true")
+else:
+    print("false")
