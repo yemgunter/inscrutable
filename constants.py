7db1c384df8216a5f3db7527d411b7794c7c0af7
Yolanda Gunter <yemgunter@gmail.com>
2021-02-15 14:31:21 -0600

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,12 @@
+
+
+name = input('Enter your name? ')
+address = input('Enter your address with city, state and ZIP ')
+phone = input('Enter your telephone number: ')
+major = input('What is your college major? ')
+
+print('Hello, ', name,'.')
+print('Your address is: ', address,'.')
+print('We can call you at ', phone,' to talk about ', major,'.')
+
+
