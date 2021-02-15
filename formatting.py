45420bc31aa41961a97156cf8bf6e3156087dc4f
Yolanda Gunter <yemgunter@gmail.com>
2021-02-15 11:35:27 -0600

    Update formatting.py

--- formatting.py
+++ formatting.py
@@ -4,14 +4,16 @@
 value = 1.345678
 
 
-print(format("Value",'10'), "Word")
-print(format(value, '10.3f'))
+##print(format("Value",'10'), "Word")
+##print(format(value, '10.3f'))
 
 #Add commas
 
-value = 123456789.345678
-num = 5437691
-
-print(format("Value",'10'), "Word")
-print(format(value, '10,.3f')) #comma after the 10
-print(format(num, ',d')) #thousands separator
+##value = 123456789.345678
+##num = 5437691
+##
+##print(format("Value",'10'), "Word")
+##print(format(value, '10,.3f')) #comma after the 10
+##print(format(num, ',d')) #thousands separator
+##
+##
