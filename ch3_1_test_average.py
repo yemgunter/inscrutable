f6b1c9e6f3a1f51958f530144b8f03c152eaba9b
Yolanda Gunter <yemgunter@gmail.com>
2021-02-15 23:11:01 -0600

    Create ch3_1_test_average.py

---
+++ ch3_1_test_average.py
@@ -0,0 +1,31 @@
+### This program will determine your test average and indicate if
+### you have an average >90
+##
+### Get three test scores
+##test1 = int(input('Enter your test first score: '))
+##test2 = int(input('Enter your test second score: '))
+##test3 = int(input('Enter your test third score: '))
+##
+### Calculate the average.
+##average = (test1 + test2 + test3) / 3
+##
+### Is this a high average?
+##if average > 90:
+##    print('You are doing great!')
+##
+### Display the average.
+##print('Your average is', average)
+
+
+##x = 1
+##y = 2
+##if x > y:
+##    print('Yes')
+##else:
+##    print('No Buddy')
+
+
+##if x > y:
+##    max=x
+##else:
+##    max=y
