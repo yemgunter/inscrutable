744f5710df36ed3fb84698d24ad5f8d200e46e10
Yolanda Gunter <yemgunter@gmail.com>
2021-03-08 13:03:17 -0600

    Update loops1.py

--- loops1.py
+++ loops1.py
@@ -1,5 +1,28 @@
-# Repetition Structures also knnown as loops
+### Repetition Structures also knnown as loops
+## while condition: (test for T/F value)
+##    stmt (repeat as long as condition is true)
+##    ....
+##    stmt
+##    
 
-while condition:
-    stmt
-    
+count = 0
+while count < 3:
+    print("Hello")
+    count = count + 1
+
+print("Round 1")
+
+count = 0
+while count < 5:
+    print("Hello, have a nice day!")
+    count += 1
+
+print("Round 2")
+
+
+count = 5
+while count > 0:
+    print("Hello, have a nice day!")
+    count -= 1
+
+print("Bye")
