f6694f47de27cfdb73cd76d0e4418536db047b53
Yolanda Gunter <yemgunter@gmail.com>
2021-03-09 21:41:15 -0600

    Create functions1.py

---
+++ functions1.py
@@ -0,0 +1,20 @@
+# this program does not take any input
+
+
+def main():
+    
+    print("This program will call a function")
+
+    user_message = input("Enter a message: ")
+    
+    message(user_message)
+
+    print("The function has been called")
+
+def message(msg):
+    print(msg)
+
+
+main()
+
+
