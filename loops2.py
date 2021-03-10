d0762aa48d730c16fc46f0f57149a34205484d23
Yolanda Gunter <yemgunter@gmail.com>
2021-03-09 21:41:01 -0600

    Update loops2.py

--- loops2.py
+++ loops2.py
@@ -1,4 +1,24 @@
-product = 0 
-while product < 100:
-    number = int(input('Enter a number: '))
-    product = number * 10
+##product = 0 
+##while product < 100:
+##    number = int(input('Enter a number: '))
+##    product = number * 10
+##
+##print("Program End")
+##
+##
+##again = 'y' 
+##while again == 'y':# Write the loop statement. 
+##    num1 = float(input('Enter a number: '))
+##    num2 = float(input('Enter another number: '))
+##    sum = num1 + num2
+##    print ('The sum of the numbers you entered is', sum)
+##    again = input('Would you like to try this operation again? (y/n): ')
+##    
+##print("Program Ended")
+
+n = 0
+total = 0
+k = 1
+while k <= n:
+    total+= k**3
+    k+=1
