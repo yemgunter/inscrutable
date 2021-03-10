c95ad1c1a89afbac446d9bc9248c054ecfccde09
Yolanda Gunter <yemgunter@gmail.com>
2021-03-09 21:41:25 -0600

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,18 @@
+
+
+def main(): # This is calling the function
+    add_numbers()
+
+# Function name
+def add_numbers(): # first define function
+    # Read two integers
+    num1 = int(input("Enter and interget: "))
+    num1 = int(input("Enter and interget: "))
+
+    # Calculate abd display the sumn
+    total = num1 + num2
+    print("The sum is", total)
+
+
+
+main()
