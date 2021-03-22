2f78c4c5d05c9596ae0b81c0ed529f9f91074366
Yolanda Gunter <yemgunter@gmail.com>
2021-03-22 01:37:19 -0500

    Ch 5 Reviewing Functions
    Defining and Calling a Void Function first - reviewing and setting  lecture notes in book walk through order
    
    Why - well this was my 2nd wk on my new job; CEH hrs; CTCM training week. :smile:

--- functions1.py
+++ functions1.py
@@ -1,20 +1,41 @@
-# this program does not take any input
+## Defining and Calling a FUNCTION Ch 5.2 Void Function
 
+
+### FIRST, define a function named message.
+##def message():
+##    print('I am Arthur')
+##    print('King of Zoos')
+##    
+### CALL the message function
+##message()
+
+# FIRST, define a function named main.
 
 def main():
+    print('I have a message for you.')
     
-    print("This program will call a function")
-
-    user_message = input("Enter a message: ")
+def message():
+    print('I am Arthur')
+    print('King of Zoos')
     
-    message(user_message)
+# CALL the message function
+message()
 
-    print("The function has been called")
+# this program does not take any input
 
-def message(msg):
-    print(msg)
-
-
-main()
-
+##def main():
+##    
+##    print("This program will call a function")
+##
+##    user_message = input("Enter a message: ")
+##    
+##    message(user_message)
+##
+##    print("The function has been called")
+##
+##def message(msg):
+##    print(msg)
+##
+##
+##main()
 
