745de3d5f10b82123c493f8e5952e9ea3f42410b
Yolanda Gunter <yemgunter@gmail.com>
2021-03-26 22:01:06 -0500

    Chapter 5 Study

--- pass_arguments2functions.py
+++ pass_arguments2functions.py
@@ -4,9 +4,19 @@ def main():
     val_1 = int(input('Enter an integer: '))
     val_2 = int(input('Enter another integer: '))
     multiply(val_1, val_2)
+# Passing a value to an argument using parameter variables
+    value = 5
+    show_double(value)
 
 def multiply(val_1, val_2):
     product = val_1 * val_2
     print ('The result is', product)
 
+# Passing a value to an argument 
+def show_double(number):
+	result = number * 2
+	print(result)
+
 main()
+
+
