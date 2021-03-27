6fb32f840b351cefabe1eff04d48142667f6c129
Yolanda Gunter <yemgunter@gmail.com>
2021-03-27 10:49:03 -0500

    Finish Chapter 5
    Updated learned processeses

--- pass_arguments2functions.py
+++ pass_arguments2functions.py
@@ -1,22 +1,45 @@
 # Multiply two integers and display the result in a function
 
+##def main():
+##    val_1 = int(input('Enter an integer: '))
+##    val_2 = int(input('Enter another integer: '))
+##    multiply(val_1, val_2)
+### Passing a value to an argument using parameter variables
+##    value = 5
+##    show_double(value)
+##
+##def multiply(val_1, val_2):
+##    product = val_1 * val_2
+##    print ('The result is', product)
+##
+### Passing a value to an argument 
+##def show_double(number):
+##	result = number * 2
+##	print(result)
+##
+##main()
+
+
+##  Write the definition of a function printGrade,
+##  which takes one parameter containing a string value and returns nothing.
+##  The function prints "Grade: " followed by the string parameter.
+
+##def printGrade(grade):
+##    print("Grade:", grade)
+
+# Create a global variable
+number = 0
+
+# Define main function
 def main():
-    val_1 = int(input('Enter an integer: '))
-    val_2 = int(input('Enter another integer: '))
-    multiply(val_1, val_2)
-# Passing a value to an argument using parameter variables
-    value = 5
-    show_double(value)
+    global number
+    number = int(input('Enter a number: '))
+    show_number()
 
-def multiply(val_1, val_2):
-    product = val_1 * val_2
-    print ('The result is', product)
+def show_number():
+    print('The number you entered is', number)
 
-# Passing a value to an argument 
-def show_double(number):
-	result = number * 2
-	print(result)
-
+# Call the main function
 main()
 
 
