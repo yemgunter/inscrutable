4bd409ec2f665458b816cb52595fb819d9127c0e
Yolanda Gunter <yemgunter@gmail.com>
2021-04-04 17:47:44 -0500

    Ch 6 file read to return content

---
+++ files_read.py
@@ -0,0 +1,24 @@
+##  Study of Chapter 6 Files and Exeptions
+##  Better late than never.  I will do much better on this Exam 2
+##  than I did on Exam 1. Conquer this and RULE my mind.
+
+
+#   This program writes three lines of data
+#   to a file.
+
+def main():
+    #   Open a file amed prophets.txt.
+    outfile = open('prophets.txt', 'w')
+
+    #   Write the names of three prophets
+    #   to the file.
+    outfile.write('John Eckhardt\n')
+    outfile.write('Matthew Stevenson\n')
+    outfile.write('Yolanda Gunter\n')
+
+    #   Close the file.
+    outfile.close()
+
+
+main()
+
