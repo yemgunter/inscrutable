bccfb28d210ecf27516d57ba3507ffe202c03051
Yolanda Gunter <yemgunter@gmail.com>
2021-04-04 14:36:17 -0500

    Open file, write to file

--- files.py
+++ files.py
@@ -3,4 +3,22 @@
 ##  than I did on Exam 1. Conquer this and RULE my mind.
 
 
+#   This program writes three lines of data
+#   to a file.
+
+def main():
+    #   Open a file amed prophets.txt.
+    oitfile = open('prophets.txt', 'w')
+
+    #   Write the names of three prophets
+    #   to the file.
+    outfile.write('John Eckhardt\n')
+    outfile.write('Matthew Stevenson\n')
+    outfile.write('Yolanda Gunter\n')
+
+    #   Close the file.
+    outfile.close()
+
+
+main()
 
