bdccf4d9b4adbdfe8590299cc209023ec1effcbb
Yolanda Gunter <yemgunter@gmail.com>
2021-04-05 21:49:09 -0500

    Create write random #s file
    Ch 6.2 using loops to process files

---
+++ write_rand_numbers.py
@@ -0,0 +1,16 @@
+## This program writes 100 random numbers to a file
+
+
+import random
+
+# Open a file.
+myfile = open('numbers.txt', 'w')
+
+# Write 100 random numbers to the file.
+for count in range(100):
+    numbers = random.randint(1,100)
+    myfile.write(str(number) + '\n')
+
+# Close the file.
+myfile.close()
+print('Done')
