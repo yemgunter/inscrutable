13e727dc741c1d3ad1ea635cb33152cd47a2838c
Yolanda Gunter <yemgunter@gmail.com>
2021-04-08 17:55:03 -0500

    Ch 6 Study Stop

--- write_rand_numbers.py
+++ write_rand_numbers.py
@@ -1,6 +1,6 @@
-## This program writes 100 random numbers to a file
-
-
+#### This program writes 100 random numbers to a file
+##
+##
 import random
 
 # Open a file.
@@ -8,9 +8,12 @@ myfile = open('numbers.txt', 'w')
 
 # Write 100 random numbers to the file.
 for count in range(100):
-    numbers = random.randint(1,100)
+    number = random.randint(1,100)
     myfile.write(str(number) + '\n')
 
 # Close the file.
 myfile.close()
 print('Done')
+
+
+
