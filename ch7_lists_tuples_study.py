32e76ee6a5aa0d33f78342538e2806492afc0aa2
Yolanda Gunter <yemgunter@gmail.com>
2021-04-08 21:10:23 -0500

    Update Ch 7 Lists study

--- ch7_lists_tuples_study.py
+++ ch7_lists_tuples_study.py
@@ -2,4 +2,40 @@
 
 # List of integers
 
-even_numbers = [2, 4, 6, 8, 10]
+##even_numbers = [2, 4, 6, 8, 10]
+##
+##numbers = list(range(1, 10, 2))
+##
+##print(numbers)
+
+
+##numbers = [0] * 5
+##print(numbers)
+##
+##
+##
+##numbers = [99, 100, 101, 102]
+##for n in numbers:
+##    print(n)
+##
+##
+
+my_list = [10, 20, 30, 40]
+
+##print(my_list[0], my_list[1], my_list[2], my_list[3])
+##
+##index = 0
+##while index < 4:
+##    print(my_list[index], '\n')
+##    index += 1
+
+##my_list = [10, 20, 30, 40]
+##print(my_list[-1], my_list[-2], my_list[-3], my_list[-4])
+
+
+# This code will cause an IndexError exception.
+my_list = [10, 20, 30, 40]
+index = 0
+while index < 5:
+    print(my_list[index])
+    index += 1
