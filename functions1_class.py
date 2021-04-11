03f579d49c3a59232289a2c7d53f515c8d1b3068
Yolanda Gunter <yemgunter@gmail.com>
2021-04-10 21:55:33 -0500

    File from Functions Lecture
    Online Class Sessions Tuesday 03/09/2021 functions1.py

---
+++ functions1_class.py
@@ -0,0 +1,17 @@
+
+def main():
+    
+    print("This program will call a function")
+
+    user_message = input("Enter a message ")
+
+    message(user_message)
+
+    print("The function has been called")
+    
+
+def message(msg):
+    print(msg)
+
+
+main()
