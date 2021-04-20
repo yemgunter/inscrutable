c1fc0d0f7b57de18afa7deac8978e6782ac560d1
Yolanda Gunter <yemgunter@gmail.com>
2021-04-20 09:07:03 -0500

    Wrapping up Ch 7 Studies

--- ch7_lists_tuples_study7.py
+++ ch7_lists_tuples_study7.py
@@ -1,3 +1,33 @@
-## Ch 7 Lists and Tuples
+## Ch 7 Lists and Tuples  7.7 barista_pay.py
+
+# This progam calculates the gross pay for
+# each of Yolanda's designers.
+
+# NUM_EMPLOYEES is used as a constant for the
+# size of the list
+NUM_EMPLOYEES = 6
+
+def main():
+    # Create a list to hold employee hours
+    hours = [0] * NUM_EMPLOYEES
+
+    # Get each employees's hours worked.
+    for index in range(NUM_EMPLOYEES):
+        print('Enter the hours worked by employee ',
+              index + 1, ': ', sep='', end='')
+        hours[index] = float(input())
+
+        # Get the hourly pay rate.
+        pay_rate = float(input('Enter the hourly pay rate: '))
+
+        # Display each employee's gross pay.
+        for index in range(NUM_EMPLOYEES):
+            gross_pay = hours[index] * pay_rate
+            print('Gross pay for employee ', index + 1, ': $',
+                  format(gross_pay, ',.2f'), sep='')
+
+# Call the main function.
+main()
+        
 
 
