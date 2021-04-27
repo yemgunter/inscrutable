1696958bdb384ca5a235da9da40e7215131069fd
Yolanda Gunter <yemgunter@gmail.com>
2021-04-26 21:17:34 -0500

    Update Lab4a testing debugging

--- lab4a_yolanda_gunter.py
+++ lab4a_yolanda_gunter.py
@@ -3,12 +3,11 @@
 #  Lab 4             
 # My program uses decisions, repetition, functions, files, lists
 # and exception handling that will get the input from a file to
-# run program that
-# greet User with what season they were born in and if they were 
-# born in a leap year or not. Then my program will ask for how many
-# pennies they have in their penny jar and calculate and display
-# the number of each money denomination they will get when they cash them in.
-# 
+# run program that asks User for current date, reads a contact file
+# list taht contains names with DOB, calculate each contact's age,
+# season contact was born in and if they were born in a leap year or not.
+# Then my program will print the output of the information with the
+# calculated average age of contacts in the data file.
 ###############################################################
 
 
@@ -16,108 +15,133 @@
 # Function name: main
 # Input: None
 # Output: print User's greeting, birth season & if leap yr or not 
-# Purpose: This function get the input from a file,
-# store the information into two lists 
-
-season they were 
-# born in and if they were born in a leap year or not.    
+# Purpose: This function reads a data file of contacts with DOB, then prints
+# a table of each contact's name, age, season born in and if they were born
+# in a leap year or not with a separate line printed of the
+# calculated average age of contacts in the data file.
 ###############################################
 def main():
+
+    month_tuple = (" ", "January", "February", "March", "April",
+                       "May", "June", "July", "August", "September",
+                       "October", "November", "December")
+
+    # Create empty name list
+    name = []
+
+    # Create empty birthdate list
+    birthdate = []
+
+    # Get current date
+    date = input('Enter current date in format m/d/yyyy: ')
+    today = date.split('/')
+    while date != 'zzz':
+        try:
+            
+            # Open a file named contactlab4.txt
+            infile = open('contactsLab4.txt', 'r')
+
+            # Read first record's name field
+            name = infile.readlines()
+            # Read rest of the file
+            while name != '':
+                nlist
+                # Read the birthdate field
+                birthdate = infile.read()
+                date_list = birthdate.split('/')
+                
+            # Strip the \n from name
+            name = name.rstrip('\n')
+
+            
     
-    # Open a file named contactlab4.txt
-    update_contacts = open('contactsLab4.txt', 'r')
-    
-    while first_name != 'zzz':
+            # Call the find_season function
+            season = find_season(month_tuple)
+            
+            # Call the is_leap_year funcion
+            year = is_leap_year(birthdate)
+            date_list = birthdate.split('/')
 
-        # Call the get_month function
-        month = get_month()
-        
-        # Call the get_year function
-        birth_year = get_year()
-        
-        # Call the find_season function
-        season = find_season(month)
-        
-        # Call the is_leap_year funcion
-        year = is_leap_year(birth_year)
-        
-        # Display greeting to User, season of birth and
-        # if birth year was leap or not
-        print("Hello, ", first_name, "! You were born in the ",
-              season, " and ", birth_year, " was ", year, sep='')
+            # Call the get_age function
 
+            age = get_age(date)
+            today = date.split('/')
+                
+            # Call the display_contacts functions
+            display_contacts(name, birthdate)
 
-        first_name = input('Enter another first name or ' \
-                           'Enter zzz to stop: ')
+            
 
-###############################################
-# Function name: get_month
-# Input: None
-# Output: value returned as month
-# Purpose: This function validates input and stores as month    
-###############################################
+# Close the file 
+            infile.close()
+        except IOError:
+            print('An error occured trying to read')
+            print('the file', infile)           
 
-def get_month():
-    month = int(input('Enter numeric birth month between 1 and 12: '))
-    # Validate the month entered
-    while month < 1 or month > 12:
-        print('ERROR: Month must be between 1 and 12')
-        month = int(input('Enter a valid month between 1 and 12: '))
-    return month
-        
-###############################################
-# Function name: get_year
-# Input: None
-# Output: User's birth year
-# Purpose: This function validates input and stores as year    
-###############################################
-
-def get_year():
-    birth_year = int(input('Enter valid birth year Ex: 1986: '))
-    # Validate the year entered
-    while birth_year < 0 or birth_year == 0:
-        print('ERROR: Year cannot be 0')
-        birth_year = int(input('Enter valid birth year Ex: 1986: '))
-    return birth_year
-
-
+            date = int(input('Enter current date in format ' \
+                               'm/d/yyyy or Enter end to stop: ')) 
 ###############################################
 # Function name: find_season
-# Input: month
-# Output: season
-# Purpose: Determines which season User is born 
+# Input: birthdate
+# Output: a string as a season
+# Purpose: Determines which season contact is born 
 ###############################################
 
-def find_season(month):
-    # Assign User's birth month to a season 
-    if month == 12 or month == 1 or month == 2:
+def find_season(month_tuple):
+    # Assign contact birth month to a season
+    month_tuple = date_list[0]
+    if month_tuple == 12 or month_tuple == 1 or month_tuple == 2:
         season = "winter"
-    elif month == 3 or month == 4 or month == 5:
+    elif month_tuple == 3 or month_tuple == 4 or month_tuple == 5:
         season = "spring"
-    elif month == 6 or month == 7 or month == 8:
+    elif month_tuple == 6 or month_tuple == 7 or month_tuple == 8:
         season = "summer"
-    elif month == 9 or month == 10 or month == 11:
+    elif month_tuple == 9 or month_tuple == 10 or month_tuple == 11:
         season = "fall"
     return season
+    
+    
 
 ###############################################
 # Function name: is_leap_year
-# Input: User's birth year
-# Output: value leap year (true) or not (False) 
+# Input: birthdate 
+# Output: value leap year (Yes) or not (No) 
 # Purpose: Determines if birth year is leap year or not.    
 ###############################################
-
-def is_leap_year(birth_year):
+def is_leap_year(birthdate):
+    date_list = birthdate.split('/')
+    year = date_list[int(birth_date[2])]
+            
     # Calculate if User's birth year is a leap year or not
-    if birth_year % 4 == 0 and birth_year % 100 != 0 or \
-       birth_year % 400 == 0:
-        year = "a leap yaer."
+    if birthdate % 4 == 0 and birthdate % 100 != 0 or \
+    birthdate % 400 == 0:
+        year = "Yes"
     else:
-        year = "not a leap year."
+        year = "No"
     return year
 
+###############################################
+# Function name: get_age
+# Input: None
+# Output: age of contact 
+# Purpose: Caculates age of contact    
+###############################################
+def get_age(date):
+    date -= birthdate[2]
+    
 
+###############################################
+# Function name: display_contacts
+# Input: birth_year
+# Output: value leap year (Yes) or not (No) 
+# Purpose: Determines if birth year is leap year or not.    
+###############################################
+def display_contacts(name, birthdate):
+    for name in 
 
+    
+            
+    
 
 # Call the main function
 main()
