20f83399ed2870dcdc367dbac3b36215fc9a0030
Yolanda Gunter <yemgunter@gmail.com>
2021-04-26 23:26:07 -0500

    Ch 8 Strings study

--- ch8_1_strings_basics.py
+++ ch8_1_strings_basics.py
@@ -1,6 +1,67 @@
+## Ch 8.1 Strings for loop accessing individual characters in a string
+
+
+## Write an expression whose value is the character at index 3 of string s.
+#s[3]
+
+## Write an expression whose value is the last character in string s.
+#s[-1]
 
 
 
-name = 'Juliet'
-for ch in name:
-    print(ch)
+##name = 'Juliet'
+##for ch in name:
+##    print(ch)
+
+##name = 'Juliet'
+##for ch in name:
+##    ch = 'X'
+##    print(name)
+
+##my_string = 'Roses are red'
+##ch = my_string[6]
+##print(ch)
+##print(my_string[0], my_string[6], my_string[10])
+
+
+##city = 'Boston'
+##print(city[6])
+
+##city = 'Boston'
+##index = 0
+##while index < 6:
+##    print(city[index])
+##    index += 1
+
+
+##city = 'Boston'
+##size = len(city)
+##print(size)
+
+
+# Concatenate Strings
+
+# Write an expression that is the concatenation of the
+# strings "Hello" and "World".
+##'Hello' + 'World'
+
+
+## Write an expression that is the concatenation of two strings s1 and s2.
+#s1 + s2
+
+
+## Write an expression whose value is the
+## concatenation of the three strigs
+## name1, name2, and name3, separated by commas.
+## So if name1, name2, and name3, were (respectively)
+## "Neville", "Dean", and "Seamus",
+## your expression's value would be "Neville,Dean,Seamus".
+#name1+','+name2+','+name3
+
+def main():
+    name = 'Yolanda'
+    print('The name is', name)
+    name = name + ' Gunter'
+    print('Now the name is', name)
+
+main()
---
+++ ch8_2_strings_slicing.py
@@ -0,0 +1,39 @@
+##first_name = "Clovis"
+##last_name = "Bagwell"
+##
+##full_name = first_name + ' ' + last_name
+##
+##print(full_name)
+
+
+##first_name = input('Enter your first name: ')
+##
+##last_name = input('Enter your last name: ')
+##
+##print('Hello', first_name, last_name)
+##
+
+# nested function
+##hours = int(input('How nany hours did you work? '))
+##pay_rate = float(input('What is your hourly pay rate? '))
+##pay = hours * pay_rate
+##
+##print('Your pay is' , pay)
+
+
+# This program gets and item's original price and
+# calculates its sale price, with a 20% discount.
+
+# Get the item's original price
+original_price = float(input("Enter the item's original price: "))
+
+# Calculate the amunt of the discount
+discount = original_price * 0.2
+
+ #Calculate the new price with discount
+sale_price = original_price - discount
+
+# Display the item's sale price
+print("The sale price is" , sale_price)
+
+
