b43e6eb061ab528c4641279a7e8b982db61255ab
Yolanda Gunter <yemgunter@gmail.com>
2021-05-01 02:14:38 -0500

    Ch 10 Lecture 4/30 Thurs

--- ch10_2_class_coin_flip.py
+++ ch10_2_class_coin_flip.py
@@ -1,8 +1,32 @@
 # Ch 10.2 Classes Coin Flip
 
-class coin
+import coin
 
+def main():
+    # create an object from the Coin class
 
+    call_the_toss()
+
+def call_the_toss():
+    # Get prediction from two players
+    player1_guess = input("heads or tails? ")
+    player2_guess = input("heads or tails? ")
+
+    # toss the coin
+    a_coin = Coin()
+    a_coin.toss()
+
+    # decide who got it right and display result
+    if player1_guess == a_coin.get_sideup():
+        print("Player 1 is correct")
+    else:
+        print("Payer 1 is not correct")
+
+    if player2_guess == a_coin.get_sideup():
+        print("Player 2 is correct")
+    else:
+        print("Payer 2 is not correct")
+  
 
 main()
     
